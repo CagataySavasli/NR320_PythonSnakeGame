e9aea59b8a15418797e15ba3c0a92bf98db5ed74
Batuhaann <batuhanbogukoglu_10@hotmail.com>
2021-12-28 00:02:55 +0300

    Game class is added

--- src/snakegame/game/Game.py
+++ src/snakegame/game/Game.py
@@ -3,31 +3,31 @@ import husnu_bey
 import NeroCup
 import ui
 
-def __init__(self):
-    pygame.init()
-    pygame.display.set_caption("Codebasics husnu_bey And Nero Cup Game")
+class Game:
 
-    pygame.mixer.init()
-    self.play_background_music()
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("Codebasics husnu_bey And Nero Cup Game")
 
-    self.surface = pygame.display.set_mode((1000, 800))
-    self.husnu_bey = husnu_bey(self.surface)
-    self.husnu_bey.draw()
-    self.neroCup = NeroCup(self.surface)
-    self.neroCup.draw()
-    self.showScreen = ui.UI(self.surface)
+        pygame.mixer.init()
+        self.play_background_music()
 
+        self.surface = pygame.display.set_mode((1000, 800))
+        self.husnu_bey = husnu_bey(self.surface)
+        self.husnu_bey.draw()
+        self.neroCup = NeroCup(self.surface)
+        self.neroCup.draw()
+        self.showScreen = ui.UI(self.surface)
 
-def play_background_music(self):
-    pygame.mixer.music.load('resources/bg_music_1.mp3')
-    pygame.mixer.music.play(-1, 0)
+    def play_background_music(self):
+        pygame.mixer.music.load('resources/bg_music_1.mp3')
+        pygame.mixer.music.play(-1, 0)
 
+    def play_sound(self, sound_name):
+        if sound_name == "crash":
+            sound = pygame.mixer.Sound("resources/crash.mp3")
+        elif sound_name == 'ding':
+            sound = pygame.mixer.Sound("resources/ding.mp3")
 
-def play_sound(self, sound_name):
-    if sound_name == "crash":
-        sound = pygame.mixer.Sound("resources/crash.mp3")
-    elif sound_name == 'ding':
-        sound = pygame.mixer.Sound("resources/ding.mp3")
-
-    pygame.mixer.Sound.play(sound)
-    # pygame.mixer.music.stop()
+        pygame.mixer.Sound.play(sound)
+        # pygame.mixer.music.stop()
