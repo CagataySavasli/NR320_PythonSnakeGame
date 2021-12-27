7373907e75b3719cfd1093c5857f8010f9f30762
Batuhaann <batuhanbogukoglu_10@hotmail.com>
2021-12-28 01:10:36 +0300

    ui class is updated

--- src/snakegame/ui/ui.py
+++ src/snakegame/ui/ui.py
@@ -57,3 +57,68 @@ class UI():
             self.parent_screen.blit(fontobject.render(message, 1, (255, 255, 255)),
                         ((self.parent_screen.get_width() / 2) - 100, (self.parent_screen.get_height() / 2) - 10))
         pygame.display.flip()
+
+        def ask(self, question):
+            "ask(screen, question) -> answer"
+            pygame.font.init()
+            current_str_un = []
+            self.show_box(question + ": " + self.str.join(current_str_un))
+            running = True
+            while running:
+                inkey = self.get_key()
+                if inkey == K_BACKSPACE:
+                    current_str_un = current_str_un[0:-1]
+                elif inkey == K_RETURN:
+                    break
+                elif inkey == K_LSHIFT or inkey == K_RSHIFT:
+                    inkey = self.get_key()
+                    if inkey <= 127:
+                        upper_case = inkey - 32
+                        current_str_un.append(chr(upper_case))
+                elif inkey <= 127:
+                    current_str_un.append(chr(inkey))
+                self.show_box(question + ": " + self.str.join(current_str_un))
+            return self.str.join(current_str_un)
+            # password
+            global display_p
+            global current_str_p
+            current_str_p = []
+            display_p = []
+            pro = ''.join(display_p)
+            display_box(screen, question + ": " + pro)
+            while 1:
+                inkey = get_key()
+                if inkey == K_BACKSPACE:
+                    current_str_p = current_str_p[0:-1]
+                    display_p = display_p[0:-1]
+                elif inkey == K_RETURN:
+                    ##      print(display_p))
+                    break
+                elif inkey == K_LSHIFT or inkey == K_RSHIFT:
+                    inkey = get_key()
+                    if inkey <= 127:
+                        upper_case = inkey - 32
+                        for character in current_str_p:
+                            display_p.append(symbol)
+                        pro = ''.join(display_p)
+                        display_box.blit(screen, question + ": " + pro)
+                        current_str_p.append(chr(upper_case))
+                elif inkey <= 127:
+                    for character in current_str_p:
+                        display_p.append(symbol)
+                    pro = ''.join(display_p)
+                    display_box.blit(screen, question + ": " + pro)
+                    current_str_p.append(chr(inkey))
+
+            ##  return string.join(display_p)
+            return display_p
+
+        ##  return string.join(current_string_p)
+
+        def login_panel(self):
+            global display_p
+            display_p = []
+            userName = self.ask("Username")
+            password = self.ask("Password")
+            if not ([userName, password] in self.userPass):
+                self.login_panel()
