ebbaf701e55152d46bb09f9dbfe95429bb6459cc
Batuhaann <batuhanbogukoglu_10@hotmail.com>
2021-12-27 23:47:03 +0300

    husnu_bey class is added

---
+++ src/snakegame/husnu_bey.py
@@ -0,0 +1,38 @@
+import pygame
+
+SIZE = 40
+
+class husnu_bey:
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.image = pygame.image.load("resources/husnu_bey.jpg").convert()
+        self.direct = 'down'
+
+        self.length = 1
+        self.x = [40]
+        self.y = [40]
+
+    def move(self, dirc):
+        self.direct = dirc
+
+
+    def move_on(self):
+        #updating husnu_bey body
+        for i in range(self.length-1,0,-1):
+            self.x[i] = self.x[i-1]
+            self.y[i] = self.y[i-1]
+
+        #updating husnu_bey head
+        if self.direct == 'left':
+            self.x[0] -= SIZE
+
+        if self.direct == 'right':
+            self.x[0] += SIZE
+
+        if self.direct == 'up':
+            self.y[0] -= SIZE
+
+        if self.direct == 'down':
+            self.y[0] += SIZE
+
+        self.draw()
