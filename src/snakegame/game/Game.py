fb18537f5189fe703fd014a0abab53fc297cf394
Batuhaann <batuhanbogukoglu_10@hotmail.com>
2021-12-28 00:19:10 +0300

    Game class is updated

--- src/snakegame/game/Game.py
+++ src/snakegame/game/Game.py
@@ -1,7 +1,9 @@
 import pygame
+from pygame.locals import *
 import NeroCup
 import husnu_bey
 import ui
+import time
 
 SIZE = 40
 
@@ -45,6 +47,84 @@ class Game:
             if y1 >= y2 and y1 < y2 + SIZE:
                 return True
         return False
+
+    def play(self):
+        self.showScreen.render_background()
+        self.husnu_bey.move_on()
+        self.neroCup.draw()
+        self.show_score()
+        pygame.display.flip()
+
+        #husnu_bey eats nero cup
+        for i in range(self.husnu_bey.length):
+            if self.is_collision(self.husnu_bey.x[i], self.husnu_bey.y[i], self.neroCup.x, self.neroCup.y):
+                self.play_sound("ding")
+                self.husnu_bey.increase_husnu_bey_length()
+                self.neroCup.move()
+
+        #husnu_bey collides with itself
+        for i in range(3, self.husnu_bey.length):
+            if self.is_collision(self.husnu_bey.x[0], self.husnu_bey.y[0], self.husnu_bey.x[i], self.husnu_bey.y[i]):
+                self.play_sound('crash')
+                raise "Collision Occurred"
+
+        #husnu_bey collides with the boundries of the window
+        if not (0 <= self.husnu_bey.x[0] <= 1000 and 0 <= self.husnu_bey.y[0] <= 800):
+            self.play_sound('crash')
+            raise "Hit the boundry error"
+
+    def show_score(self):
+        font = pygame.font.SysFont('arial',30)
+        score = font.render(f"Score: {self.husnu_bey.length}",True,(200,200,200))
+        self.surface.blit(score,(850,10))
+
+    def login_panel(self):
+        self.showScreen.login_panel()
+
+    def run(self, currentDirec):
+        running = True
+        pause = False
+        self.login_panel()
+        while running:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        running = False
+
+                    if event.key == K_RETURN:
+                        pygame.mixer.music.unpause()
+                        pause = False
+
+                    if not pause:
+                        if event.key == K_LEFT and currentDirec != "right":
+                            self.husnu_bey.move("left")
+                            currentDirec = "left"
+
+                        if event.key == K_RIGHT and currentDirec != "left":
+                            self.husnu_bey.move("right")
+                            currentDirec = "right"
+
+                        if event.key == K_UP and currentDirec != "down":
+                            self.husnu_bey.move("up")
+                            currentDirec = "up"
+
+                        if event.key == K_DOWN and currentDirec != "up":
+                            self.husnu_bey.move("down")
+                            currentDirec = "down"
+
+                elif event.type == QUIT:
+                    running = False
+            try:
+
+                if not pause:
+                    self.play()
+
+            except Exception as e:
+                self.showScreen.show_game_over(self.husnu_bey.length)
+                pause = True
+                self.reset()
+
+            time.sleep(.1)
 
 if __name__ == '__main__':
     currentDirec = "down"
