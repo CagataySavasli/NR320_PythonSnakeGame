9cb1255cee3716e2a3ebb7ecbedfe17b5e8a3689
berkebilensoy <67325102+berkebilensoy@users.noreply.github.com>
2021-12-28 21:15:47 +0300

    Signin method was added. Database implementation was added. Some features were added thanks to databases. Score screen was edited.

--- src/snakegame/ui/ui.py
+++ src/snakegame/ui/ui.py
@@ -1,32 +1,44 @@
 import pygame, pygame.font, pygame.event, pygame.draw, string
 from pygame.locals import *
-import uuid
 import pygame
+import database
 
 class UI():
+    str = ''
+    current_str_p = []
+    display_p = []
+    userName = ""
+    password = ""
+    den1 = ""
+    den2 = ""
+
+    button = pygame.Rect(400, 450, 150, 50)
+    clock = pygame.time.Clock()
+    fps = 60
+    size = [200, 200]
+    bg = [255, 255, 255]
+
     def __init__(self, parent_screen):
         self.parent_screen = parent_screen
 
     def render_background(self):
-        bg = pygame.image.load("resources/background.jpg")
+        bg = pygame.image.load("src/assets/background.jpg")
         self.parent_screen.blit(bg, (0,0))
 
     def show_game_over(self, score):
         self.render_background()
         font = pygame.font.SysFont('arial', 30)
         line1 = font.render(f"Game is over! Your score is {score}", True, (255, 255, 255))
-        self.parent_screen.blit(line1, (200, 300))
+        self.parent_screen.blit(line1, (200, 280))
 
-        line2 = font.render("Press ENTER to play again. Press Escape to exit!", True, (255, 255, 255))
-        self.parent_screen.blit(line2, (200, 350))
+        maxScore = database.editScore(self.userName,score)
+        line2 = font.render(f"Your Max Score is {maxScore}", True, (255, 255, 255,))
+        self.parent_screen.blit(line2, (200, 330))
+
+        line3 = font.render("Press ENTER to play again. Press Escape to exit!", True, (255, 255, 255))
+        self.parent_screen.blit(line3, (200, 380))
         pygame.mixer.music.pause()
         pygame.display.flip()
-
-    str = ''
-    current_str_p = []
-    display_p = []
-    userPass = [["ata","ala"],["cago,sago"]]
-    button = pygame.Rect(400, 450, 150, 50)
 
     def get_key(self):
         while 1:
@@ -34,7 +46,10 @@ class UI():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = event.pos
                 if self.button.collidepoint(mouse_pos):
-                    print("Signin")
+                    # prints current location of mouse
+                    self.signin()
+                    self.show_box(self.den1,self.den2)
+
             if event.type == pygame.QUIT:
                 return pygame.quit()
             if event.type == KEYDOWN:
@@ -67,7 +82,6 @@ class UI():
                          ((self.parent_screen.get_width() / 2) - 102,
                           (self.parent_screen.get_height() / 2) - 12,
                           204, 24), 1)
-
 
         if len(message) != 0:
             self.parent_screen.blit(fontobject.render(message, 1, (255, 255, 255)),
@@ -126,15 +140,26 @@ class UI():
                     pro = ''.join(display_p)
                     show_box.blit(screen, question + ": " + pro, slTest)
                     current_str_p.append(chr(inkey))
-
-            ##  return string.join(display_p)
             return display_p
 
-        ##  return string.join(current_string_p)
-
         def login(self):
+            self.den1 = "Username"
+            self.den2 = "Login"
             self.userName = self.ask("Username", "Login")
+            self.den1 = "Password"
+            self.den2 = "Login"
             self.password = self.ask("Password", "Login")
+
+            if not database.login(self.userName, self.password):
+                self.login()
+
+        def signin(self):
+            self.userName = self.ask("Username", "Signin")
+            self.password = self.ask("Password", "Signin")
+            if not database.checkForSignin(self.userName):
+                database.signin(self.userName, self.password)
+            else:
+                self.signin()
 
         def startScreen(self):
             global display_p
