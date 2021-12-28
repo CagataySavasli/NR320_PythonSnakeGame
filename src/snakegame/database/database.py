6f83175a18af6f70b0919c12db6d28c85d823cdd
CagataySavasli <cagatay.savasli@ozu.edu.tr>
2021-12-28 18:50:35 +0300

    signin and editScore method written

--- src/snakegame/database/database.py
+++ src/snakegame/database/database.py
@@ -7,3 +7,23 @@ cursor = con.cursor()
 def createTable():
     cursor.execute("CREATE TABLE IF NOT EXISTS players (name TEXT NOT NULL UNIQUE,password TEXT,maxScore INT)")
     con.commit()
+
+def signin(name, password):
+    try:
+        cursor.execute("INSERT INTO players (name, password, maxScore) VALUES(?, ?, ?)", (name, password, 0))
+        con.commit()
+    except Exception as e:
+        return False
+
+
+def editScore(name, score):
+    cursor.execute("SELECT p.maxScore FROM players as p WHERE p.name = '{}'".format(name))
+    row = cursor.fetchall()
+    if row[0][0] < score:
+        cursor.execute("UPDATE players SET maxScore = ? WHERE name = ?", (score, name))
+        con.commit()
+        return score
+    return row[0][0]
+
+
+
